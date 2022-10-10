86d40e41e2f6598134f4f330a9fb59ea27cadd6a
Saadi-Rahman <sadi6280@gmail.com>
2022-10-10 15:20:29 +0600

    data stored in local storage and shown in the cart when the page is reloaded

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,25 +2,29 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
+    // console.log(cart);
     
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
 
     for(const product of cart){
-        total = total + product.price;
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
 
-    const tax = (total * 0.1).toFixed(2);
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
 
     return (
         <div className='cart'>
-            <h3>Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
+            <h2>Order Summary</h2>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
-            <h5>Grand Total: </h5>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
